150c67817107accdfdb2531ef18c4be190db14a9
musaubrian <musaubrian15@gmail.com>
2022-10-11 16:12:59 +0300

    Chore: implement using flags

--- cli-quiz/main.go
+++ cli-quiz/main.go
@@ -1,9 +1,14 @@
 package main
 
-func quizGame() {
-  println("Hello")
+import "flag"
+
+func cliQuiz() {
+	csvFileName := flag.String("filename", "problems.csv", "the csv file name")
+	flag.Parse()
+
+	println(*csvFileName)
 }
 
 func main() {
-  quizGame()
+	cliQuiz()
 }

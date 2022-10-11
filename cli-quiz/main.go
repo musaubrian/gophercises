ed926f952ac53d1b6602a35f6e3075e987860dba
musaubrian <musaubrian15@gmail.com>
2022-10-11 20:44:41 +0300

    Chore: finish

--- cli-quiz/main.go
+++ cli-quiz/main.go
@@ -1,14 +1,5 @@
 package main
 
-import "flag"
-
-func cliQuiz() {
-	csvFileName := flag.String("filename", "problems.csv", "the csv file name")
-	flag.Parse()
-
-	println(*csvFileName)
-}
-
 func main() {
 	cliQuiz()
 }
